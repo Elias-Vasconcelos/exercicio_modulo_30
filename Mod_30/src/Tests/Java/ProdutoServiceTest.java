34c99f64aa8e9a38f92d0b0b7890a0e79fc4bacd
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2025-01-06 19:03:07 -0300

    ajustes

---
+++ Mod_30/src/Tests/Java/ProdutoServiceTest.java
@@ -0,0 +1,60 @@
+package Tests.Java;
+
+import Tests.Java.dao.ProdutoDaoMock;
+import main.Java.br.com.Elias.Dao.IProdutoDAO;
+import main.Java.br.com.Elias.Domain.Produto;
+import main.Java.br.com.Elias.Services.IProdutoService;
+import main.Java.br.com.Elias.Services.ProdutoService;
+import main.Java.br.com.Elias.exceptions.DAOException;
+import main.Java.br.com.Elias.exceptions.TipoChaveNaoEncontradaException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class ProdutoServiceTest {
+
+	private IProdutoService produtoService;
+	
+	private Produto produto;
+	
+	public ProdutoServiceTest() {
+		IProdutoDAO dao = new ProdutoDaoMock();
+		produtoService = new ProdutoService(dao);
+	}
+	
+	@Before
+	public void init() {
+		produto = new Produto();
+		produto.setCodigo("A1");
+		produto.setDescricao("Produto 1");
+		produto.setNome("Produto 1");
+		produto.setValor(BigDecimal.TEN);
+	}
+	
+	@Test
+	public void pesquisar() throws DAOException {
+		Produto produtor = this.produtoService.consultar(produto.getCodigo());
+		Assert.assertNotNull(produtor);
+	}
+	
+	@Test
+	public void salvar() throws TipoChaveNaoEncontradaException, DAOException {
+		Boolean retorno = produtoService.cadastrar(produto);
+		Assert.assertTrue(retorno);
+	}
+	
+	@Test
+	public void excluir() throws DAOException {
+		produtoService.excluir(produto.getCodigo());
+	}
+	
+	@Test
+	public void alterarCliente() throws TipoChaveNaoEncontradaException, DAOException {
+		produto.setNome("Rodrigo Pires");
+		produtoService.alterar(produto);
+		
+		Assert.assertEquals("Rodrigo Pires", produto.getNome());
+	}
+}
